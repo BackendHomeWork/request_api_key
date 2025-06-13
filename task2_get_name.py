7667fd8a8e30dffb62b14df9803cef8f0d061492
Sanjarbek Saidov <71938677+Sanjarbek17@users.noreply.github.com>
2025-06-13 09:48:44 +0500

    Update task2_get_name.py

--- task2_get_name.py
+++ task2_get_name.py
@@ -3,12 +3,15 @@ import requests
 API_KEY = "YOUR_API_KEY_HERE"
 
 
-def get_random_name():
+def get_random_name(nameType:str, quantity: int):
     """
     Make a GET request to the Randommer API to get a random name.
 
     This function should:
     - Send a GET request to: https://randommer.io/api/Name
+    - With parameter nameType and quantity
+    - nameType = one of these ("firstname" "surname" "fullname")
+    - quantity = number of names
     - Include the API key in the X-Api-Key header
     - Print the random name from the response
     """
